d72fb14a701ef1e9b9f8c04b23a066ca62df1b58
Yvonne <ys359375@gmail.com>
2025-06-23 23:25:51 +0300

    create landing page

---
+++ lite_lms_react/src/components/LandingHeader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './LandingHeader.css'; // optional styling
+
+function LandingHeader() {
+  return (
+    <header className="landing-header">
+      <div className="logo">Lite LMS</div>
+      <nav className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </nav>
+    </header>
+  );
+}
+
+export default LandingHeader;
---
+++ lite_lms_react/src/pages/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import LandingHeader from '../components/LandingHeader';
+import LandingHero from '../components/LandingHero';
+import LandingFooter from '../components/LandingFooter';
+
+function LandingPage() {
+  return (
+    <div>
+      <LandingHeader />
+      <LandingHero />
+      <LandingFooter />
+    </div>
+  );
+}
+
+export default LandingPage;
