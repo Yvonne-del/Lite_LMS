461ded70186ba8618088e5805385cbc851dc689e
william <williamwambugu663@gmail.com>
2025-06-27 09:31:48 +0300

    start.sh

--- server/config.py
+++
@@ -1,9 +0,0 @@
-# import os
-# from dotenv import load_dotenv
-
-# # Load environment variables from my .env file
-# load_dotenv()
-class config:
-    JWT_SECRET_KEY = "vkdmkmsisahajasijgdbahgajkdj"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///./lms.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- server/database.py
+++
@@ -1,9 +0,0 @@
-# database.py
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "sqlite:///./lms.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
