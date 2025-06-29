81244e19127b52af40013fd030d80ca37bb64a1f
Yvonne <ys359375@gmail.com>
2025-06-29 19:37:38 +0300

    .

---
+++ server/schemas.py
@@ -0,0 +1,103 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import datetime
+from datetime import date
+
+#------users-------
+class UserBase(BaseModel):
+    email: EmailStr
+    name: str 
+    role: str = "student"  # 'student' or 'teacher'
+    model_config = {
+        "from_attributes": True
+    }
+    
+class UserCreate(UserBase):
+    password: str
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+
+class UserOut(UserBase):
+    id: int
+
+    model_config = {
+        "from_attributes" : True
+    }
+#------courses-------
+class CourseBase(BaseModel):
+    name: str
+    code: str
+    semester: Optional[int] = None
+
+class CourseCreate(CourseBase):
+    teacher_id: int
+
+class CourseOut(CourseBase):
+    id: int
+    teacher_id: int
+
+    model_config = {
+        "from_attributes" : True
+    }
+#------lessons-------
+class LessonBase(BaseModel):
+    title: str
+    content: Optional[str] = None
+    video_url: Optional[str] = None
+
+class LessonCreate(LessonBase):
+    course_id: int
+
+class LessonUpdate(BaseModel):
+    title: Optional[str] = None
+    content: Optional[str] = None
+
+class LessonOut(LessonBase):
+    id: int
+    course_id: int
+
+model_config = {
+        "from_attributes" : True
+    }
+#------assignments-------
+class AssignmentBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+    due_date: Optional[date] = None
+
+    model_config = {
+        "from_attributes": True
+    }
+
+class AssignmentCreate(AssignmentBase):
+    course_id: int
+
+class AssignmentOut(AssignmentBase):
+    id: int
+    course_id: int
+
+    model_config = {
+        "from_attributes" : True
+    }
+#------submissions-------
+
+class SubmissionBase(BaseModel):
+    file_path: Optional[str] = None
+
+class SubmissionCreate(SubmissionBase):
+    user_id: int
+    assignment_id: int
+
+class SubmissionOut(BaseModel):
+    id: int
+    file_path: str
+    timestamp: datetime
+    reviewed: bool
+    user_id: int
+    assignment_id: int
+
+    model_config = {"from_attributes": True}
+
